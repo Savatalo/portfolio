54935aeb3263a4232a97d837c77df810f5ebb821
Marko Savic <130310373+Savatalo@users.noreply.github.com>
2025-12-06 19:06:53 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import React, { useRef } from "react";
 import Social from './components/Social.tsx'
 import Terminal from './components/Terminal.tsx'
 import AsciiClock from './components/AsciiClock.tsx'
 import './App.css'
 
 function App() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleTerminate = () => {
+    console.log("Terminal closed");
+  };
 
   return (
     <>
     <div className='main-div'>
      <Social/>
-     <Terminal/>
+     <Terminal onTerminate={handleTerminate} inputRef={inputRef} />
     </div>
     <AsciiClock/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
